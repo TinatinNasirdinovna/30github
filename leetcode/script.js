99d87060651073bdf1476d27bb5341b15bb940ae
TinatinNasirdinovna <asankulovatinatin2002@gmail.com>
2025-02-17 22:15:27 +0600

    new sait

--- leetcode/script.js
+++ leetcode/script.js
@@ -66,7 +66,18 @@
 //     return Object.values(groups)
 //   };
 
-// ?
+// ? Given an array nums of size n, return the majority element.
+
+// The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+// Example 1:
+
+// Input: nums = [3,2,3]
+// Output: 3
+// Example 2:
+
+// Input: nums = [2,2,1,1,1,2,2]
+// Output: 2
 
 // const majorityElement = function (nums) {
 //     nums.sort()
