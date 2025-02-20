45e668f55a5aa35f54ed0d2853095047ac1aad84
TinatinNasirdinovna <asankulovatinatin2002@gmail.com>
2025-02-20 23:03:59 +0600

    leetcode

--- leetcode/script.js
+++ leetcode/script.js
@@ -114,4 +114,33 @@
 //   return stack.length === 0;
 // };
 
+// ? Given a sorted integer array nums and an integer n, add/patch elements to the array such that any number in the range [1, n] inclusive can be formed by the sum of some elements in the array.
 
+// Return the minimum number of patches required.
+
+// const minPatches = function (nums, n) {
+//     let miss = 1;
+//     let result = 0;
+//     let i = 0;
+
+//     while (miss <= n) {
+//         if (i < nums.length && nums[i] <= miss) {
+//             miss += nums[i];
+//             i++;
+//         } else {
+//             miss += miss;
+//             result++;
+//         }
+//     }
+
+//     return result;
+// };
+
+// ?
+
+let nam = "Tinatin";
+let res = "";
+for (let i = 0; i < nam.length; i++) {
+  res += nam[i] + "-";
+}
+console.log(res);
