510ebde15fe97daae2a3dcede4439657a376c181
TinatinNasirdinovna <asankulovatinatin2002@gmail.com>
2025-03-02 22:07:13 +0600

    style

--- italian_cuisine/src/comnonents/pages/mainPage/components/bestSeller/BestSeller.tsx
+++ italian_cuisine/src/comnonents/pages/mainPage/components/bestSeller/BestSeller.tsx
@@ -2,7 +2,13 @@ import React from "react";
 import s from "./BestSeller.module.scss";
 
 const BestSeller = () => {
-  return <section id={s.seller}></section>;
+  return (
+    <section id={s.seller}>
+      <div className="container">
+        <div className={s.bestSeller}></div>
+      </div>
+    </section>
+  );
 };
 
 export default BestSeller;
