77bdaeec772f9df89c7f9f7a77bde21f10ff8599
TinatinNasirdinovna <asankulovatinatin2002@gmail.com>
2025-03-05 22:26:26 +0600

    dom

--- dom/script.js
+++ dom/script.js
@@ -5,11 +5,12 @@ const lists = document.querySelector(".lists");
 btn.addEventListener("click", () => {
   let list = document.createElement("li");
   list.innerHTML = input.value;
+  list.className = "list";
   let btn = document.createElement("button");
   btn.innerHTML = "delete";
   let edit = document.createElement("button");
   edit.innerHTML = "Edit";
-  lists.append(edit);
-  lists.append(btn);
   lists.append(list);
+  list.append(edit);
+  list.append(btn);
 });
